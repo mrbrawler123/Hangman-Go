128bb02895e3b6dc817132bf5deb2a956b82ad87
mrbrawler <omkaar.nerurkar@gmail.com>
2021-02-23 03:18:44 +0000

    formatting removed

--- main.py
+++ main.py
@@ -3,18 +3,14 @@ import time
 nltk.download("words")
 nltk.download("wordnet")
 print("\033[2J\033[H")
-
-
 def find_letters(letter, word):
-	index = []
-	for i in range(len(word)):
-		if letter == word[i]:
-			index.append(i)
-		if letter != word[i]:
-			continue
-	return index
-
-
+  index = []
+  for i in range(len(word)):
+    if letter == word[i]:
+      index.append(i)
+    if letter != word[i]:
+      continue
+  return index
 time.sleep(1)
 print("HANGMAN GO")
 time.sleep(3)
@@ -53,31 +49,23 @@ while True:
 			print(f"Congratulations! You guessed the word in {tries} chances!")
 			break
 		if len(s) == 1 and word.count(s) > 0:
-			for a in find_letters(s, word):
+			for a in find_letters(s,word):
 				answer[a] = s
 				correct = correct + 1
 			print(f"Yay! You guessed correctly!")
 			if correct == len(word):
-				print(
-				    f"Congratulations! You guessed the word in {tries} chances!"
-				)
+				print(f"Congratulations! You guessed the word in {tries} chances!")
 				break
 			if tries == len(word) + 5:
 				print(f"You have lost! The word was {word}!")
-			print(
-			    f"You have {len(word) + 5 - tries} chances remaining. You have used: {used_final}"
-			)
+			print(f"You have {len(word) + 5 - tries} chances remaining. You have used: {used_final}")
 		if word.count(s) == 0:
-			tries = tries + 1
+			tries = tries + 1  
 			print(f"You guessed wrong!")
 			if correct == len(word):
-				print(
-				    f"Congratulations! You guessed the word in {tries} chances!"
-				)
+				print(f"Congratulations! You guessed the word in {tries} chances!")
 				break
 			if tries == len(word) + 5:
 				print(f"You have lost! The word was {word}!")
-			print(
-			    f"You have {len(word) + 5 - tries} chances remaining. You have used: {used_final}"
-			)
+			print(f"You have {len(word) + 5 - tries} chances remaining. You have used: {used_final}")  
 		final_answer = separator.join(answer)
